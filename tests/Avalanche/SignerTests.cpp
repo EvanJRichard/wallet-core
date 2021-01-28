267b320e1bfb2019372a39b7c63b380cd159275f
Evan Richard <evan@avalabs.org>
2021-01-28 09:41:36 -0500

    Add skeleton signer tests.

--- tests/Avalanche/SignerTests.cpp
+++ tests/Avalanche/SignerTests.cpp
@@ -15,9 +15,9 @@
 using namespace TW;
 using namespace TW::Avalanche;
 
-// TODO: Add tests
+// These tests are modeled after the avalanchejs signer tests.
 
-TEST(AvalancheSigner, Sign) {
+TEST(AvalancheSigner, SignWithAsofLocktimeThreshold) {
     // TODO: Finalize test implementation
 
     //auto key = PrivateKey(parse_hex("__PRIVKEY_DATA__"));
@@ -29,6 +29,82 @@ TEST(AvalancheSigner, Sign) {
     //auto signature = Signer::sign(key, transaction);
     //auto result = transaction.serialize(signature);
 
-    //ASSERT_EQ(hex(serialized), "__RESULT__");
+    //ASSERT_EQ(hex(serialized), "");
     //ASSERT_EQ(...)
 }
+
+TEST(AvalancheSigner, SignWithoutAsofLocktimeThreshold) {
+}
+
+TEST(AvalancheSigner, SignOperationTx) {
+
+}
+
+TEST(AvalancheSigner, SignImportTx) {
+}
+
+TEST(AvalancheSigner, SignExportTx) {
+}
+
+
+/*
+  test('Creation Tx1 with asof, locktime, threshold', () => {
+    const txu:UnsignedTx = set.buildBaseTx(
+      netid, blockchainID,
+      new BN(9000), assetID, addrs3, addrs1, addrs1, undefined, undefined, undefined,
+      UnixNow(), UnixNow().add(new BN(50)), 1,
+    );
+    const tx:Tx = txu.sign(keymgr1);
+
+    const tx2:Tx = new Tx();
+    tx2.fromString(tx.toString());
+    expect(tx2.toBuffer().toString('hex')).toBe(tx.toBuffer().toString('hex'));
+    expect(tx2.toString()).toBe(tx.toString());
+  });
+  test('Creation Tx2 without asof, locktime, threshold', () => {
+    const txu:UnsignedTx = set.buildBaseTx(
+      netid, blockchainID,
+      new BN(9000), assetID,
+      addrs3, addrs1, addrs1
+    );
+    const tx:Tx = txu.sign(keymgr1);
+    const tx2:Tx = new Tx();
+    tx2.fromBuffer(tx.toBuffer());
+    expect(tx2.toBuffer().toString('hex')).toBe(tx.toBuffer().toString('hex'));
+    expect(tx2.toString()).toBe(tx.toString());
+  });
+
+  test('Creation Tx3 using OperationTx', () => {
+    const txu:UnsignedTx = set.buildNFTTransferTx(
+      netid, blockchainID, 
+      addrs3, addrs1, addrs2, nftutxoids, new BN(90), avaxAssetID, undefined,
+      UnixNow(), UnixNow().add(new BN(50)), 1,
+    );
+    const tx:Tx = txu.sign(keymgr1);
+    const tx2:Tx = new Tx();
+    tx2.fromBuffer(tx.toBuffer());
+    expect(tx2.toBuffer().toString('hex')).toBe(tx.toBuffer().toString('hex'));
+  });
+
+  test('Creation Tx4 using ImportTx', () => {
+    const txu:UnsignedTx = set.buildImportTx(
+      netid, blockchainID, addrs3, addrs1, addrs2, importUTXOs, bintools.cb58Decode(PlatformChainID), new BN(90), assetID,
+      new UTF8Payload("hello world").getPayload(), UnixNow());
+    const tx:Tx = txu.sign(keymgr1);
+    const tx2:Tx = new Tx();
+    tx2.fromBuffer(tx.toBuffer());
+    expect(tx2.toBuffer().toString('hex')).toBe(tx.toBuffer().toString('hex'));
+  });
+
+  test('Creation Tx5 using ExportTx', () => {
+    const txu:UnsignedTx = set.buildExportTx(
+      netid, blockchainID, new BN(90), avaxAssetID,
+      addrs3, addrs1, addrs2, bintools.cb58Decode(PlatformChainID), 
+      undefined, undefined, new UTF8Payload("hello world").getPayload(), UnixNow()
+    )
+    const tx:Tx = txu.sign(keymgr1);
+    const tx2:Tx = new Tx();
+    tx2.fromBuffer(tx.toBuffer());
+    expect(tx.toBuffer().toString('hex')).toBe(tx2.toBuffer().toString('hex'));
+  });
+*/
